8d8138abbf24b7abc677dbd8e3a6e4f0aaff63ab
Sione Havili <sionehavili2@gmail.com>
2023-11-16 21:40:12 -0700

    updated method to post

--- src/components/PostContactInfo.jsx
+++ src/components/PostContactInfo.jsx
@@ -1,31 +1,22 @@
-
-
-const PostContactInfo = async ({ dataToPost }) => 
-{
-  try 
-  {
+const PostContactInfo = async ({ dataToPost }) => {
+  try {
     const response = await fetch(
       `https://portfolio-web3350-default-rtdb.firebaseio.com/contacts.json`,
       {
-        method: "PUT",
+        method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(dataToPost),
       }
     );
 
-    if (!response.ok) 
-    {
-        console.log("PostContactInfo FAILED to post data to database...");
-        return false;
+    if (!response.ok) {
+      console.log("PostContactInfo FAILED to post data to database...");
+      return false;
+    } else {
+      console.log("PostContactInfo successful...", response);
+      return true;
     }
-    else
-    {
-        console.log("PostContactInfo successful...", response);
-        return true;
-    }
-
-  } catch (error) 
-  {
+  } catch (error) {
     console.error("PostContactInfo failed:", error);
     return false;
   }
